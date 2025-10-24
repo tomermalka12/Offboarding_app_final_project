ac7e9ee0bbcd2864611976887514c32dc977590b
Tomer Malka Pinto <tomermalka12@gmail.com>
2025-10-24 11:52:43 +0300

    html tamplate modify

--- offboarding_app/app.py
+++ offboarding_app/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/", methods=["GET"])
 def index():
